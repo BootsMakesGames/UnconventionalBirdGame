04e162fc13e14be07994ad561a5058cb04569e1c
BootsMakesGames <156555817+BootsMakesGames@users.noreply.github.com>
2026-09-07 21:55:28 -0600

    set up dialogue input events

--- scripts/dialogue_ui.gd
+++ scripts/dialogue_ui.gd
@@ -8,6 +8,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+
+
+func _on_next_arrow_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_prev_arrow_pressed() -> void:
+	pass # Replace with function body.
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- scripts/dialogue_view.gd
+++ scripts/dialogue_view.gd
@@ -1,19 +1,40 @@
 extends Node2D
 
+# Ready UI elements
 @onready var character = %CharacterSprite
 @onready var textbox = %DialogueUI
 
+# Set current dialogue line to 0
+var current_line : int
+const FIRST_LINE = 0
+
+# FROM TUTORIAL
+# TODO: Replace with some kind of READ function
 const TEMP_DIALOGUE : Array[String] = [
 	"Dodo: I've truly become a silly little guy lately.",
 	"Dodo: I'm the king of water cup city!",
-	"Dodo: Productivity does not determine my worth."
+	"Dodo: Productivity does not determine my worth.",
+	"Dodo: The bird was touched by the gods themselves.",
+	"Dodo: By <i>the bird</i> I mean me."
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Process first line of dialogue
-	process_line(parse_line(TEMP_DIALOGUE[0]))
+	current_line = FIRST_LINE
+	process_current_line() 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("next_line"):
+		if current_line < len(TEMP_DIALOGUE) -1:
+			current_line += 1
+			process_current_line()
+	elif event.is_action_pressed("prev_line"):
+		if current_line > FIRST_LINE:
+			current_line -= 1
+			process_current_line()
+	else:
+		pass
 
 func parse_line(line: String):
 	var line_info = line.split(":")
@@ -23,6 +44,8 @@ func parse_line(line: String):
 		"dialogue_text": line_info[1]
 	}
 	
-func process_line(line_info: Dictionary):
+func process_current_line():
+	var line = TEMP_DIALOGUE[current_line]
+	var line_info = parse_line(line)
 	textbox.speaker_name.text = line_info["speaker_name"]
 	textbox.dialogue_text.text = line_info["dialogue_text"]
