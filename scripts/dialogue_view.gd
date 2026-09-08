5d91698f1223da5f6981ad254bb38cc8d9cb2a2f
BootsMakesGames <156555817+BootsMakesGames@users.noreply.github.com>
2026-09-07 20:23:56 -0600

    pass dialogue text to UI

--- scripts/dialogue_view.gd
+++ scripts/dialogue_view.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+@onready var character = %CharacterSprite
+@onready var textbox = %DialogueUI
+
+const TEMP_DIALOGUE : Array[String] = [
+	"Dodo: I've truly become a silly little guy lately.",
+	"Dodo: I'm the king of water cup city!",
+	"Dodo: Productivity does not determine my worth."
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Process first line of dialogue
+	process_line(parse_line(TEMP_DIALOGUE[0]))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func parse_line(line: String):
+	var line_info = line.split(":")
+	assert(len(line_info) >= 2)
+	return {
+		"speaker_name": line_info[0],
+		"dialogue_text": line_info[1]
+	}
+	
+func process_line(line_info: Dictionary):
+	textbox.speaker_name.text = line_info["speaker_name"]
+	textbox.dialogue_text.text = line_info["dialogue_text"]
