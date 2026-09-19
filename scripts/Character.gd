4dbee5424e2db558f164df533a56984dfaa26dea
BootsMakesGames <156555817+BootsMakesGames@users.noreply.github.com>
2026-09-18 23:03:39 -0600

    basic character class setup

---
+++ scripts/Character.gd
@@ -0,0 +1,24 @@
+class_name Character
+extends Node
+
+## FROM TUTORIAL ##
+enum Name {
+	DODO
+}
+
+const CHARACTER_DETAILS: Dictionary = {
+	Name.DODO: {
+		"name": "Dodo",
+		"species": "Dodo",
+		"sprite": preload("res://scenes/character_sprite.tscn"),
+		"affinity_points": 0
+	}
+}
+
+static func get_enum_from_string(string_value: String) -> int:
+	var upper_string = string_value.to_upper()
+	if Name.has(upper_string):
+		return Name[upper_string]
+	else:
+		push_error("Invalid character name: " + string_value)
+		return -1
